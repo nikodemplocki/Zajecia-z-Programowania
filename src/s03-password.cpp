75e5f147ef17d1b48d4be5efb4099d8adda6825f
Nikodem Płocki <s24258@pwjstk.edu.pl>
2021-10-15 12:57:26 +0200

    zadanie05

---
+++ src/s03-password.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+auto main( int argc, char* argv[]) -> int
+{
+	std::string haslo_wpisane;
+
+	if(argc <= 1)
+	{
+		std::cout << "Blad. Brak podanego argumentu";
+		return 1;
+	}
+
+	std::string password = std::string(argv[1]);
+
+do
+{
+	std::cout << "Password: ";
+	std::cin >> haslo_wpisane;
+}while (password != haslo_wpisane);
+std::cout << "ok! \n";
+
+return 0;
+
+}
