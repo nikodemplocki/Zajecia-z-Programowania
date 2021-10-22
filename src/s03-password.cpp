fcbfcd0fabb307411690567112060211180c19f9
Nikodem Plocki <s24258@pjwstk.edu.pl>
2021-10-22 10:17:56 +0200

    zad 5

--- src/s03-password.cpp
+++ src/s03-password.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
-#include <ctime>
+
 
 auto main(int argc, char* argv[]) -> int
 {
- 	std::cout << argv[0];
-	for (auto i = 1; i , argc; ++i) {
-		std::cout << " " << argv[i];
+ 	std::string p_password;
+
+	if(argc <= 1)
+	{
+		std::cout << "brak podanego argumentu";
+	return 1;
+
+	}
+	std::string password = std::string(argv[1]);
+
+	do
+	{
+		std::cout << "password: ";
+		std::cin >> p_password;
+	}while (password != p_password);
+	std::cout << "ok! \n";
+
+	return 0;
 }
-std::cout << "\n";
